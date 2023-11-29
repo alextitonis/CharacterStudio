2fd18be0eb3b0ea3080e06a5ec603b53d551298d
memelotsqui <memelotsqui@gmail.com>
2023-11-28 19:19:13 -0600

    extract json read file from react component

--- src/pages/Appearance.jsx
+++ src/pages/Appearance.jsx
@@ -10,6 +10,7 @@ import { AudioContext } from "../context/AudioContext"
 import FileDropComponent from "../components/FileDropComponent"
 import { getFileNameWithoutExtension } from "../library/utils"
 import { getTraitOption } from "../library/option-utils"
+import { getDataArrayFromNFTMetadata } from "../library/file-utils"
 
 function Appearance({
   animationManager,
@@ -115,68 +116,15 @@ function Appearance({
     if (file && file.name.toLowerCase().endsWith('.vrm')) {
       handleVRMDrop(file);
     } 
-
-    const filesArray = Array.from(files);
-    const jsonDataArray = [];
-    const processFile = (file) => {
-      return new Promise((resolve, reject) => {
-        if (file && file.name.toLowerCase().endsWith('.json')) {
-          const reader = new FileReader();
-          const thumbLocation = `${templateInfo.assetsLocation}/anata/_thumbnails/t_${file.name.split('_')[0]}.jpg`;
-          const jsonName = file.name.split('.')[0];
-
-          reader.onload = function (e) {
-            try {
-              const jsonContent = JSON.parse(e.target.result);
-              const options = [];
-              const jsonAttributes = jsonContent.attributes.map((attribute) => (
-                { trait: attribute.trait_type, id: attribute.value }
-                )).filter((item) => item.trait !== "TYPE" && 
-                                    item.trait !== "BRACE" &&
-                                    item.trait !== "SET" &&
-                                    item.trait !== "SPECIAL_OTHER" );
-
-              jsonContent.attributes.forEach((attribute) => {
-                  options.push(getTraitOption(attribute.value, attribute.trait_type, templateInfo));
-              });
-
-              const filteredOptions = options.filter((element) => element !== null);
-
-              templateInfo.traits.forEach((trait) => {
-                const coincidence = filteredOptions.some((option) => option.trait.trait === trait.trait);
-                if (!coincidence) {
-                  filteredOptions.push({ item: null, trait: trait });
-                }
-              });
-
-              const jsonSelection = { name: jsonName, thumb: thumbLocation, attributes: jsonAttributes, options: filteredOptions };
-              jsonDataArray.push(jsonSelection);
-
-              resolve(); // Resolve the promise when processing is complete
-            } catch (error) {
-              console.error("Error parsing the JSON file:", error);
-              reject(error);
-            }
-          };
-
-          reader.readAsText(file);
+    if (file && file.name.toLowerCase().endsWith('.json')) {
+      getDataArrayFromNFTMetadata(files, templateInfo).then((jsonDataArray)=>{
+        if (jsonDataArray.length > 0){
+          // This code will run after all files are processed
+          setJsonSelectionArray(jsonDataArray);
+          setSelectedOptions(jsonDataArray[0].options);
         }
-      });
-    };
-
-  // Use Promise.all to wait for all promises to resolve
-  Promise.all(filesArray.map(processFile))
-  .then(() => {
-    if (jsonDataArray.length > 0){
-      // This code will run after all files are processed
-      setJsonSelectionArray(jsonDataArray);
-      setSelectedOptions(jsonDataArray[0].options);
-    }
-  })
-  .catch((error) => {
-    console.error("Error processing files:", error);
-  });
-
+      })
+    } 
   };
 
   return (
